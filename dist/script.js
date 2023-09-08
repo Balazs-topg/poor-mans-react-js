2dfcabbf7d2eef276b06e78946c90360385219df
Balazs Hevesi <balazshevesi13@gmail.com>
2023-09-08 12:16:00 +0000

    Setup tsx dev enviorment

---
+++ dist/script.js
@@ -0,0 +1,6 @@
+console.log("typescript is workingg");
+const React = {};
+function testingJSX() {
+    return (React.createElement("div", null, "testing"));
+}
+console.log(testingJSX());
---
+++ script.tsx
@@ -0,0 +1,12 @@
+console.log("typescript is workingg")
+
+const React = {
+}
+
+function testingJSX(): any{
+    return (
+        <div>
+            testing
+        </div>)
+}
+console.log(testingJSX())
